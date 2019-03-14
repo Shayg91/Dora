e4f9c08bb5f064be7380b41d31cd128e37c0148b
EdenSalman <edensalman22@gmail.com>
2019-03-14 17:07:41 +0200

    Add scenario basics

--- dora/src/Scenarios.js
+++ dora/src/Scenarios.js
@@ -1,41 +1,144 @@
 import React, { Component } from 'react';
-import './Scenarios.css'
 import firebase from './scripts/Dora';
 import Grid from '@material-ui/core/Grid'
+import { TextField, Button, MenuItem, Paper, Snackbar, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close'
+
+import './Scenarios.css'
 
 import Scenario from './partials/Scenario';
 
 class Scenarios extends Component{
     constructor(props) {
         super(props);
-        
-        this.scenarios = ['1','2','3','4'];
 
-        //this.ref = firebase.firestore().collection('scenario');
+        this.state ={
+            scenarios: [],
+            ref: firebase.firestore().collection('scenario'),
+            add_new: true,
+            added: false,
+            data: {
+                level: 1,
+                action : 1,
+                affectPath: '',
+            }
+        }
 
-        /*this.singleLesson = this.ref.doc('U9c0DAnBIl6JznrAjkfa')
-            .get()
-            .then((doc)=> this.setState({goals: doc.data().goals}));*/
+        this.handleToggleClick = this.handleToggleClick.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleFieldChange = this.handleFieldChange.bind(this);
+
+        this.getAlScenarios();
     }
 
-    updateInput = e => {
-        this.senarios.setState({
-            [e.target.name]: e.target.value
-        });
-    };
+    getAlScenarios(){
 
-    addScenario = e => {
-        e.preventDefault();
-        this.scenarios.push("new-item");
-    };
+        let currentComponent = this;
+
+        this.state.ref.get().then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                currentComponent.setState(state => ({
+                    scenarios: [...state.scenarios, doc],
+                }));
+            });
+        });
+
+    }
+
+    handleToggleClick() {
+        this.setState(state => ({
+            add_new: !state.add_new
+        }));
+    }
+
+    handleSubmit(event) {
+        this.setState(state => ({
+            added: !state.added
+        }));
+        event.preventDefault();
+    }
+
+    handleFieldChange = (field) => (event) => {
+        let data = { ...this.state.data };
+        data[field] = event.target.value;
+        this.setState({ data });
+    }
+
+    handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        this.setState({ added: false });
+    }
 
     render(){
         return(
-            <div>
+            <div className='main'>
                 <h3>Scenarios</h3>
                 <Grid container direction='column' justify='space-around' alignItems='stretch'>
-                    <Scenario data={this.scenarios}/>
+                    {!this.state.add_new ?
+                        <Paper className='paper'>
+                            <Grid container direction="column" justify="center" alignItems="flex-start" spacing={16}>
+                                <form >
+                                    <TextField id="Action" label="Select action:" select value={this.state.data.action}
+                                               onChange={this.handleFieldChange('action')} margin="normal">
+                                        <MenuItem key='1' value='1'>
+                                            Action 1
+                                        </MenuItem>
+                                        <MenuItem key='2' value='2'>
+                                            Action 2
+                                        </MenuItem>
+                                        <MenuItem key='3' value='3'>
+                                            Action 3
+                                        </MenuItem>
+                                    </TextField >
+                                    <br/>
+                                    <TextField id="level" label="Level" select value={this.state.data.level}
+                                               onChange={this.handleFieldChange('level')} margin="normal">
+                                        <MenuItem key='1' value='1'>
+                                            Level 1
+                                        </MenuItem>
+                                        <MenuItem key='2' value='2'>
+                                            Level 2
+                                        </MenuItem>
+                                        <MenuItem key='3' value='3'>
+                                            Level 3
+                                        </MenuItem>
+                                    </TextField>
+                                    <Button variant="contained" color='secondary' onClick={this.handleSubmit}>
+                                        Save
+                                    </Button>
+                                </form>
+                            </Grid>
+                        </Paper>
+                        : null
+                    }
+                    <Button variant="contained" className='add-new-btn' onClick={this.handleToggleClick}>
+                        {this.state.add_new ? 'Add New Scenario' : 'Close' }
+                    </Button>
+                    <Scenario data={this.state.scenarios}/>
                 </Grid>
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'right',
+                    }}
+                    open={this.state.added}
+                    color='secondary'
+                    autoHideDuration={6000}
+                    onClose={this.handleClose}
+                    message={<span id="message-id">Scenario Added Successfully!</span>}
+                    scenario={[
+                        <IconButton
+                            key="close"
+                            aria-label="Close"
+                            color="inherit"
+                            onClick={this.handleClose}
+                        >
+                            <CloseIcon />
+                        </IconButton>,
+                    ]}
+                />
             </div>
         );
     }
--- dora/src/partials/Scenario.js
+++ dora/src/partials/Scenario.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
+import { Typography, ButtonBase } from '../../node_modules/@material-ui/core';
 
-import NewScenario from './NewScenario';
+import './Scenario.css'
 
 class Scenario extends Component{
     constructor(props) {
         super(props);
-
-        console.log(this.props);
     }
 
-    render(){
-        return(<Grid container direction='row' spacing={24}>
-            {this.props.data.map(function(scenario){
-            return  (<Grid item xs={3}>
-                        <Paper>{scenario}</Paper>
-            </Grid>)})
-            }
-            <NewScenario addScenario={this.addScenario}/>
-        </Grid>
-        )};
+    render() {
+        return (
+            <div>
+                {this.props.data.map(doc => (
+                    <Paper key={doc.id} className='paper'>
+                        <Grid container spacing={16}>
+                            <Grid item>
+                                <ButtonBase className='image'>
+                                    <img className='img' alt="complex" src={doc.data().affectPath} />
+                                </ButtonBase>
+                            </Grid>
+                            <Grid item xs={6} sm container>
+                                <Grid item xs container direction="column" spacing={16}>
+                                    <Grid item xs>
+                                        <Typography gutterBottom variant="subtitle1">
+                                            {doc.data().textOrWAV}
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography style={{ cursor: 'pointer' }}>Remove</Typography>
+                                    </Grid>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant="subtitle1">{doc.data().level}</Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default Scenario;
