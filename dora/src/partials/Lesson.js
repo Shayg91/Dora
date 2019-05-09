bf2f78c5a2829549617b564ba08d124638d5b960
Bar Ezra <barezra94@gmail.com>
2019-05-09 18:20:43 +0300

    fix(Lesson): Remove duplicate reference to Card

--- dora/src/partials/Lesson.js
+++ dora/src/partials/Lesson.js
@@ -6,7 +6,6 @@ import {
   CardMedia,
   CardHeader,
   CardContent,
-  Card,
   IconButton
 } from "../../node_modules/@material-ui/core";
 
@@ -31,49 +30,50 @@ class Lesson extends Component {
           title={this.props.data.title}
           subheader={this.props.data.category}
         />
-       <CardMedia image={this.props.data.scenarios[0].name} />
-       <CardContent>
-        <Typography component="p">
-          {this.props.data.scenarios[0].name}
-        </Typography>
-      </CardContent>
-      {// <div>
-      //   {this.props.data.map(doc => (
-      //     <Paper key={doc.id} className="paper">
-      //       <Grid container spacing={16}>
-      //         <Grid item>
-      //           <ButtonBase className="image">
-      //             <img
-      //               className="img"
-      //               alt="complex"
-      //               src={doc.data().affectPath}
-      //             />
-      //           </ButtonBase>
-      //         </Grid>
-      //         <Grid item xs={6} sm container>
-      //           <Grid item xs container direction="column" spacing={16}>
-      //             <Grid item xs>
-      //               <Typography gutterBottom variant="subtitle1">
-      //                 {doc.data().title}
-      //               </Typography>
-      //             </Grid>
-      //             <Grid item>
-      //               <Typography style={{ cursor: "pointer" }}>
-      //                 Remove
-      //               </Typography>
-      //             </Grid>
-      //           </Grid>
-      //           <Grid item>
-      //             <Typography variant="subtitle1">
-      //               {doc.data().level}
-      //             </Typography>
-      //           </Grid>
-      //         </Grid>
-      //       </Grid>
-      //     </Paper>
-      //   ))}
-      // </div>
-      }
+        <CardMedia image={this.props.data.scenarios[0].name} />
+        <CardContent>
+          <Typography component="p">
+            {this.props.data.scenarios[0].name}
+          </Typography>
+        </CardContent>
+        {
+          // <div>
+          //   {this.props.data.map(doc => (
+          //     <Paper key={doc.id} className="paper">
+          //       <Grid container spacing={16}>
+          //         <Grid item>
+          //           <ButtonBase className="image">
+          //             <img
+          //               className="img"
+          //               alt="complex"
+          //               src={doc.data().affectPath}
+          //             />
+          //           </ButtonBase>
+          //         </Grid>
+          //         <Grid item xs={6} sm container>
+          //           <Grid item xs container direction="column" spacing={16}>
+          //             <Grid item xs>
+          //               <Typography gutterBottom variant="subtitle1">
+          //                 {doc.data().title}
+          //               </Typography>
+          //             </Grid>
+          //             <Grid item>
+          //               <Typography style={{ cursor: "pointer" }}>
+          //                 Remove
+          //               </Typography>
+          //             </Grid>
+          //           </Grid>
+          //           <Grid item>
+          //             <Typography variant="subtitle1">
+          //               {doc.data().level}
+          //             </Typography>
+          //           </Grid>
+          //         </Grid>
+          //       </Grid>
+          //     </Paper>
+          //   ))}
+          // </div>
+        }
       </Card>
     );
   }
